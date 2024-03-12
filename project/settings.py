bc6823fc58e974a35880b9d57391806e0ac11783
= <=>
2024-03-12 14:10:33 +0530

    aws changes

--- project/settings.py
+++ project/settings.py
@@ -155,3 +155,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AWS_ACCESS_KEY_ID = 'AKIA3FLD2PWD74C2SGP2'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'acadamicfolio'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-west-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
